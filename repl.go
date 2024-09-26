b0e70c91a30edb3a264dcbdaa8ffc0917af523ff
Tyler Gregory <tylergreg95@gmail.com>
2024-09-25 21:06:02 -0400

    repl.go and tests, command_exit, command_help

---
+++ repl.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("pokedex > ")
+
+		scanner.Scan()
+		text := scanner.Text()
+
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
+
+		commandName := cleaned[0]
+
+		availableCommands := getCommands()
+
+		command, ok := availableCommands[commandName]
+		if !ok {
+			fmt.Println("Invalid command")
+			continue
+		}
+
+		command.callback()
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Prints the help menu",
+			callback:    callbackHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exits the Pokedex",
+			callback:    callbackExit,
+		},
+	}
+}
+
+func cleanInput(str string) []string {
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+	return words
+}
